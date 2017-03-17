1a4083b9e8a641c4007d0f1545a9475cd90bf466
janenevins <jane.e.nevins@gmail.com>
2017-03-17 13:29:28 -0700

    Add files via upload

---
+++ datehelper.py
@@ -0,0 +1,12 @@
+def fix_date_string(datestring):  #this function transforms the date string into something that can be correctly sorted
+    """
+    datestring change from '04/05/1984' to
+    '1984-05-04'
+    """
+    ds = datestring.split('/')
+    return '-'.join([ds[2], ds[0], ds[1]])
+
+def __sortfoo(somerecord): #this creats the key to sort by the date
+    a = somerecord['Process Date']
+    b = fix_date_string(a)
+    return b
